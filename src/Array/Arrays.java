010ca75d5a633edeff17d74c571040436ef14b21
shivansh-rajput-01 <shivanshr579@gmail.com>
2026-06-15 17:16:41 +0530

    Array in Java

---
+++ src/Array/Arrays.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class Arrays {
+    public static void main(String[] args){
+        int[] arr = new int[5]; // declaration + instantiation(memory allocation) + initialization(default 0 for int)
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+    }
+}
